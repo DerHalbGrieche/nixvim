e1a57e44c6a2f18da3a6810806d211e240d6cd45
Vasilis Manetas <vasilis12.manetas@gmail.com>
2025-04-25 17:55:34 +0200

    replace presence with neocord

--- config/presence.nix
+++ config/presence.nix
@@ -1,17 +1,26 @@
 {
-  plugins.presence-nvim = {
+  plugins.neocord = {
     enable = true;
-    autoUpdate = true;
-    logLevel = null;
-    debounceTimeout = 10;
-    enableLineNumber = false;
-    neovimImageText = "The One True Text Editor";
-    editingText = "Editing %s";
-    fileExplorerText = "Browsing %s";
-    gitCommitText = "Committing changes";
-    pluginManagerText = "Managing plugins";
-    readingText = "Reading %s";
-    workspaceText = "Working on %s";
-    lineNumberText = "Line %s out of %s";
+    settings = {
+      auto_update = true;
+      blacklist = [];
+      debounce_timeout = 10;
+      log_level = null;
+      file_assets = null;
+      enable_line_number = false;
+      global_timer = true;
+      show_time = true;
+      logo = "https://repository-images.githubusercontent.com/325421844/ecb73f47-cb89-4ee0-a0fd-9743c2f3569a";
+      main_image = "logo";
+      logo_tooltip = "NixVim";
+      editing_text = "Editing...";
+      file_explorer_text = "Browsing...";
+      line_number_text = "Line %s out of %s";
+      workspace_text = "Working on %s";
+      reading_text = "Reading...";
+      terminal_text = "Using Terminal...";
+      plugin_manager_text = "Managing plugins...";
+      git_commit_text = "Committing changes...";
+    };
   };
 }
