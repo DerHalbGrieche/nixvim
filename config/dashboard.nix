ec0f3660199c592e2b27ee7115d12fa36b4c103e
Vasilis Manetas <vasilis12.manetas@gmail.com>
2025-04-27 16:44:15 +0200

    customize dashboard

--- config/dashboard.nix
+++ config/dashboard.nix
@@ -6,6 +6,15 @@
       theme = "hyper";
       config = {
         week_header.enable = true;
+        footer = [""];
+        shortcut = [
+          {
+            action.__raw = "function() vim.cmd[[ene]] end";
+            key = "n";
+            desc = "New";
+            icon = "🆕";
+          }
+        ];
       };
     };
   };
