8438231d063a60338a5278e6155056db3d392ebb
Vasilis Manetas <vasilis.manetas@fh-erfurt.de>
2025-07-27 17:03:28 +0200

    improve rust language server setup

--- config/languages/rust.nix
+++ config/languages/rust.nix
@@ -2,28 +2,12 @@
   plugins = {
     bacon.enable = true;
     conform-nvim.settings.formatters_by_ft.rust = ["rustfmt"];
-    luasnip.fromSnipmate = [
-      {
-        paths = ../snippets/store/snippets/rust.snippets;
-        include = ["rust"];
-      }
-    ];
-    # Extra PLugin Rustacean
-    rustaceanvim = {
+
+    lsp.servers.rust_analyzer = {
       enable = true;
-      settings = {
-        tools.enable_clippy = true;
-        server = {
-          default_settings = {
-            inlayHints = {lifetimeElisionHints = {enable = "always";};};
-            rust-analyzer = {
-              cargo = {allFeatures = true;};
-              check = {command = "clippy";};
-              files = {excludeDirs = ["target" ".git" ".cargo" ".github" ".direnv"];};
-            };
-          };
-        };
-      };
+      installCargo = true;
+      installRustc = true;
+      installRustfmt = true;
     };
     lsp.servers.wgsl_analyzer.enable = true;
   };
