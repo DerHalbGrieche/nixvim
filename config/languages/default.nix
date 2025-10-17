aaf0211f14e2e28029579b251da2c273eeaee95a
Vasilis Manetas <vasilis.manetas@fh-erfurt.de>
2025-10-17 13:12:52 +0200

    smarter way to import langs

--- config/languages/default.nix
+++ config/languages/default.nix
@@ -1,14 +1,14 @@
-{pkgs, ...}: {
-  imports = [
-    ./cpp.nix
-    ./css.nix
-    ./html.nix
-    ./markdown.nix
-    ./nix.nix
-    ./python.nix
-    ./rust.nix
-    ./php.nix
-  ];
+{pkgs, ...}: let
+  # Read all files in the current directory
+  files = builtins.readDir ./.;
+
+  # Filter out default.nix and non-.nix files
+  nixFiles = builtins.filter (name: name != "default.nix" && builtins.match ".*\\.nix" name != null) (builtins.attrNames files);
+
+  # Create a list of import statements
+  imports = map (name: ./. + "/${name}") nixFiles;
+in {
+  imports = imports;
   lsp = {
     inlayHints.enable = true;
     keymaps = [
