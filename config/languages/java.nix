a81878858ef9304a624088fe5b66afb7390d8a13
Vasilis Manetas <vasilis.manetas@fh-erfurt.de>
2025-10-17 19:41:26 +0200

    add more language servers

--- config/languages/java.nix
+++ config/languages/java.nix
@@ -1,1 +1,13 @@
-{}
+{
+  lsp.servers.jdtleng = {
+    enable = true;
+    activate = true;
+    config = {
+      cmd = [
+        "jdtls"
+      ];
+      filetypes = ["java"];
+      root_markers = [".git" "mvnw" "gradlew" "pom.xml" "build.gradle"];
+    };
+  };
+}
--- config/languages/javascript.nix
+++ config/languages/javascript.nix
@@ -1,1 +1,14 @@
-{}
+{
+  lsp.servers.eslint = {
+    enable = true;
+    activate = true;
+    config = {
+      cmd = [
+        "vscode-eslint-language-server"
+        "--stdio"
+      ];
+      filetypes = ["javascript" "javascriptreact" "typescript" "typescriptreact"];
+      root_markers = [".eslintrc" ".eslintrc.js" ".eslintrc.json" "package.json"];
+    };
+  };
+}
