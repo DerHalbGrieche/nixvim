8c6947f86df3bb3ad92711ee16210592b86825a0
Vasilis Manetas <vasilis.manetas@fh-erfurt.de>
2025-11-01 12:33:09 +0100

    add support for php auto formatting

--- config/languages/conform.nix
+++ config/languages/conform.nix
@@ -23,6 +23,19 @@
       formatters_by_ft = {
         "_" = ["trim_whitespace"];
         nix = ["alejandra"];
+        php = ["prettier_html" "php-cs-fixer"];
+      };
+      formatters = {
+        php-cs-fixer = {
+          command = "php-cs-fixer";
+          args = ["fix" "$FILENAME" "--using-cache=no" "--allow-risky=yes" "--quiet"];
+          stdin = false;
+        };
+        prettier_html = {
+          command = "prettier";
+          args = ["--parser" "html"];
+          stdin = true;
+        };
       };
       format_on_save = {
         lspFallback = true;
@@ -31,5 +44,5 @@
       notify_on_error = true;
     };
   };
-  extraPackages = with pkgs; [prettier prettierd alejandra];
+  extraPackages = with pkgs; [prettier prettierd alejandra php84Packages.php-cs-fixer];
 }
