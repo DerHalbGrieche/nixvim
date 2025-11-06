8fbf6b826ad511062b81c388b0aba13cad06bc6e
Vasilis Manetas <vasilis.manetas@fh-erfurt.de>
2025-11-06 23:42:07 +0100

    enable php formatting

--- config/languages/conform.nix
+++ config/languages/conform.nix
@@ -1,47 +1,48 @@
 {pkgs, ...}: {
-  # plugins.none-ls = {
-  #   enable = true;
-  #   enableLspFormat = true;
-  #   sources = {
-  #     formatting = {
-  #       prettierd.enable = true;
-  #       prettier = {
-  #         enable = true;
-  #         settings = {
-  #           extra_filetypes = ["html" "markdown" "css" "javascript" "php"];
-  #         };
-  #       };};
-  #     formatting.alejandra.enable = true;
-  #     diagnostics.statix.enable = true;
-  #     diagnostics.deadnix.enable = true;
-  #     code_actions.statix.enable = true;
-  #   };
-  # };
-  plugins.conform-nvim = {
-    enable = true;
-    settings = {
-      formatters_by_ft = {
-        "_" = ["trim_whitespace"];
-        nix = ["alejandra"];
-        php = ["prettier_html" "php-cs-fixer"];
-      };
-      formatters = {
-        php-cs-fixer = {
-          command = "php-cs-fixer";
-          args = ["fix" "$FILENAME" "--using-cache=no" "--allow-risky=yes" "--quiet"];
-          stdin = false;
+  plugins = {
+    lsp-format.enable = true;
+    none-ls = {
+      enable = true;
+      enableLspFormat = true;
+      sources = {
+        formatting = {
+          alejandra.enable = true;
+          phpcbf.enable = true;
         };
-        prettier_html = {
-          command = "prettier";
-          args = ["--parser" "html"];
-          stdin = true;
+        diagnostics = {
+          statix.enable = true;
+          deadnix.enable = true;
+          phpstan.enable = true;
+          phpcs.enable = true;
         };
+        code_actions.statix.enable = true;
       };
-      format_on_save = {
-        lspFallback = true;
-        timeoutMs = 500;
+    };
+    conform-nvim = {
+      enable = true;
+      settings = {
+        formatters_by_ft = {
+          "_" = ["trim_whitespace"];
+          nix = ["alejandra"];
+        };
+        formatters = {
+          php-cs-fixer = {
+            command = "php-cs-fixer";
+            args = ["fix" "$FILENAME" "--using-cache=no" "--allow-risky=yes" "--quiet"];
+            stdin = false;
+          };
+          prettier_html = {
+            command = "prettier";
+            args = ["--parser" "html"];
+            stdin = true;
+          };
+        };
+        format_on_save = {
+          lspFallback = true;
+          timeoutMs = 500;
+        };
+        notify_on_error = true;
       };
-      notify_on_error = true;
     };
   };
   extraPackages = with pkgs; [prettier prettierd alejandra php84Packages.php-cs-fixer];
