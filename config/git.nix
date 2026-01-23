d06e43753fcb9c4a7de0c7b9db3167dd05dfe93e
Vasilis Manetas <vasilis.manetas@fh-erfurt.de>
2026-01-23 22:57:27 +0100

    allow git blame

--- config/git.nix
+++ config/git.nix
@@ -3,15 +3,26 @@
     neogit = {
       enable = true;
     };
+    gitsigns = {
+      enable = true;
+    };
   };
   keymaps = [
     {
       mode = "n";
-      key = "<leader>g";
+      key = "<leader>go";
       action = "<cmd>Neogit<CR>";
       options = {
         silent = true;
         desc = "Open Neogit";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action = "<cmd>Gitsigns blame_line<CR>";
+      options = {
+        desc = "Git Blame";
       };
     }
   ];
