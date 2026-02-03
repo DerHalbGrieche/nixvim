848e0e751341b49ecfd11c28d11cd3de6732410a
Vasilis Manetas <vasilis.manetas@fh-erfurt.de>
2026-02-03 12:29:57 +0100

    chore: update

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -4,20 +4,12 @@
       enable = true;
 
       nixvimInjections = true;
-      folding = true;
+      folding.enable = true;
       settings = {
         indent.enable = true;
         highlight.enable = true;
         ensure_installed = "all";
         auto_install = true;
-      };
-    };
-
-    treesitter-refactor = {
-      enable = true;
-      settings.highlight_definitions = {
-        enable = true;
-        clearOnCursorMove = false;
       };
     };
 
